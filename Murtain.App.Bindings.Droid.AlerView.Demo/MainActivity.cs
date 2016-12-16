1e0d939fd4c500115ecbdc671300e0f2d45132f9
murtain <murtain@163.com>
2016-12-16 22:23:55 +0800

    AlertView

---
+++ Murtain.App.Bindings.Droid.AlerView.Demo/MainActivity.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Com.Bigkoo.Alertview;
+using Java.Lang;
+using Android.Views.InputMethods;
+using static Android.Views.View;
+
+namespace Murtain.App.Bindings.Droid.AlerView.Demo
+{
+    [Activity(Label = "Murtain.App.Bindings.Droid.AlerView.Demo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, IOnItemClickListener, IOnDismissListener, IOnFocusChangeListener
+    {
+        int count = 1;
+        private AlertView alertView;
+        private AlertView alertViewExt;
+        private EditText alertViewEditText;
+        private InputMethodManager inputMethodManager;
+
+        public void OnDismiss(Java.Lang.Object p0)
+        {
+            this.alertView.Dismiss();
+        }
+
+        public void OnItemClick(Java.Lang.Object p0, int p1)
+        {
+            Toast.MakeText(this, $"itme click {p1}", ToastLength.Short);
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+
+            inputMethodManager = (InputMethodManager)GetSystemService(Context.InputMethodService);
+
+
+            FindViewById<Button>(Resource.Id.btnNarmal).Click += (sender, agrs) =>
+            {
+                this.alertView = new AlertView("标题", "内容", "取消", new string[] { "确定" }, null, this, AlertView.Style.Alert, this);
+                this.alertView.Show();
+            };
+
+            FindViewById<Button>(Resource.Id.btnMessageTip).Click += (sender, agrs) =>
+            {
+                this.alertView = new AlertView("标题", "内容", null, new string[] { "确定" }, null, this, AlertView.Style.Alert, this);
+                this.alertView.Show();
+            };
+            FindViewById<Button>(Resource.Id.btnActionSheet).Click += (sender, agrs) =>
+            {
+                this.alertView = new AlertView("标题", null, "取消", new string[] { "高亮按钮1" }, new string[] { "其他按钮1", "其他按钮2", "其他按钮3" }, this, AlertView.Style.ActionSheet, this);
+                this.alertView.Show();
+            };
+            FindViewById<Button>(Resource.Id.btnActionSheetSelection).Click += (sender, agrs) =>
+            {
+                this.alertView = new AlertView("上传头像", null, "取消", null, new string[] { "拍照", "从相册中选择" }, this, AlertView.Style.ActionSheet, this);
+                this.alertView.Show();
+            };
+            FindViewById<Button>(Resource.Id.btnActionSheetTip).Click += (sender, agrs) =>
+            {
+                this.alertView = new AlertView("标题", "内容", "取消", null, null, this, AlertView.Style.ActionSheet, this)
+                         .SetCancelable(true);
+                this.alertView.Show();
+            };
+            FindViewById<Button>(Resource.Id.btnExtend).Click += (sender, agrs) =>
+            {
+
+                this.alertView = new AlertView("标题", "内容", "取消", new string[] { "确定" }, null, this, AlertView.Style.Alert, this)
+                                        .SetCancelable(true)
+                                        .SetOnDismissListener(this);
+
+                this.alertViewExt = new AlertView("提示", "请完善你的个人资料！", "取消", null, new string[] { "完成" }, this, AlertView.Style.Alert, this);
+                ViewGroup extView = (ViewGroup)LayoutInflater.From(this).Inflate(Resource.Layout.AlertViewEditText, null);
+                this.alertViewEditText = (EditText)extView.FindViewById(Resource.Id.AlertViewEditText);
+
+                this.alertViewEditText.OnFocusChangeListener = this;
+
+                this.alertViewExt.AddExtView(extView);
+
+                alertViewExt.Show();
+
+            };
+        }
+        public void OnFocusChange(View v, bool hasFocus)
+        {
+            this.alertViewExt.SetMarginBottom(inputMethodManager.IsActive && hasFocus ? 120 : 0);
+
+            Toast.MakeText(this, $"InpuntMethodManager {inputMethodManager.IsActive}", ToastLength.Short).Show();
+        }
+        private void closeKeyboard()
+        {
+            inputMethodManager.HideSoftInputFromWindow(alertViewEditText.WindowToken, 0);
+            this.alertViewExt.SetMarginBottom(0);
+        }
+    }
+}
+
---
+++ Murtain.App.Bindings.Droid.PickerView.Demo/MainActivity.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Com.Bigkoo.Pickerview;
+using static Com.Bigkoo.Pickerview.TimePickerView;
+using static Com.Bigkoo.Pickerview.OptionsPickerView;
+using Com.Bigkoo.Pickerview.Model;
+using System.Collections;
+
+namespace Murtain.App.Bindings.Droid.PickerView.Demo
+{
+    [Activity(Label = "Murtain.App.Bindings.Droid.PickerView.Demo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+        , IOnTimeSelectListener
+        , IOnOptionsSelectListener
+    {
+        int count = 1;
+        private TimePickerView datePickerView;
+        private OptionsPickerView optionsPickerView;
+
+        private IList provinceCollection;
+        private IList<IList> cityCollection;
+        private IList<IList<IList>> streetCollection;
+
+        public void OnOptionsSelect(int position1, int position2, int position3)
+        {
+            Toast.MakeText(this
+                , $"{((Province)provinceCollection[position1])?.PickerViewText}-{((City)cityCollection[position1][position2])?.PickerViewText}-{((Street)streetCollection[position1][position2][position3])?.PickerViewText}"
+                , ToastLength.Short).Show();
+        }
+
+        public void OnTimeSelect(Java.Util.Date date)
+        {
+            Toast.MakeText(this, $"select date is {date.Year}-{date.Month}-{date.Day}", ToastLength.Short).Show();
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            this.datePickerView = new TimePickerView(this, TimePickerView.Type.YearMonthDay);
+            this.datePickerView.SetTime(new Java.Util.Date());
+            this.datePickerView.SetCyclic(false);
+            this.datePickerView.SetCancelable(true);
+
+            this.datePickerView.SetOnTimeSelectListener(this);
+
+            Button btnDatePicker = FindViewById<Button>(Resource.Id.btnDatePicker);
+            btnDatePicker.Click += (sender, args) =>
+            {
+                this.datePickerView.Show();
+            };
+
+            this.optionsPickerView = new OptionsPickerView(this);
+
+
+            provinceCollection = new List<Province>();
+            provinceCollection.Add(new Province("北京", "010"));
+
+            cityCollection = new JavaList<IList> {
+                new JavaList() {
+                    new City("东城区", "100010"),
+                    new City("朝阳区", "100020"),
+                    new City("西城区", "100032"),
+                    new City("通州区", "101149"),
+                }
+            };
+
+
+            streetCollection = new JavaList<IList<IList>> {
+                new JavaList<IList> {
+                    new JavaList {
+                            new Street("朝阳门"),
+                            new Street("北新桥"),
+                            new Street("和平里"),
+                        },
+                    new JavaList {
+                            new Street("朝阳区"),
+                        },
+                   new JavaList {
+                            new Street("西城区"),
+                        },
+                    new JavaList {
+                        }
+                },
+            };
+
+            optionsPickerView.SetTitle("选择城市");
+
+            this.optionsPickerView.SetPicker(provinceCollection, cityCollection, streetCollection, true);
+            optionsPickerView.SetCyclic(false, false, false);
+            optionsPickerView.SetSelectOptions(0, 0, 0);
+            this.optionsPickerView.SetOnoptionsSelectListener(this);
+
+
+
+            Button btnOptionsPickerView = FindViewById<Button>(Resource.Id.btnCascadingSelection);
+            btnOptionsPickerView.Click += (sender, args) =>
+            {
+                this.optionsPickerView.Show();
+            };
+        }
+
+        public class Province : Java.Lang.Object, IPickerViewData
+        {
+            public Province()
+            {
+
+            }
+
+            public Province(string name, string code)
+            {
+                this.Name = name;
+                this.Code = code;
+            }
+
+            public string Name { get; set; }
+            public string Code { get; set; }
+
+            public string PickerViewText
+            {
+                get
+                {
+                    return this.Name;
+                }
+            }
+        }
+
+        public class City : Java.Lang.Object, IPickerViewData
+        {
+            public City()
+            {
+
+            }
+
+            public City(string name, string code)
+            {
+                this.Name = name;
+                this.Code = code;
+            }
+
+            public string Name { get; set; }
+            public string Code { get; set; }
+
+            public string PickerViewText
+            {
+                get
+                {
+                    return this.Name;
+                }
+            }
+        }
+
+        public class Street : Java.Lang.Object, IPickerViewData
+        {
+            public Street()
+            {
+
+            }
+
+            public Street(string name)
+            {
+                this.Name = name;
+            }
+
+            public string Name { get; set; }
+
+            public string PickerViewText
+            {
+                get
+                {
+                    return this.Name;
+                }
+            }
+        }
+    }
+
+
+}
+
